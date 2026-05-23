15dc350d3b5c749cf185c46e3931d83f15091f12
Eduard0Lopes <luizeduardolopes2001@gmail.com>
2026-05-23 20:16:58 -0300

    feat: exercicios e arrays

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,9 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Principal {
     public static void main() {
         Filme meuFilmeBaseAula = new Filme();
@@ -24,20 +27,34 @@ public class Principal {
         lost.setTemporadas(10);
         lost.setEpisodiosTemporada(10);
         lost.setMinutosPorEpisodios(50);
-        System.out.println("Maratonar lost " + lost.getDuracaoEmMinutos());
+       // System.out.println("Maratonar lost " + lost.getDuracaoEmMinutos());
         CalculadoraTempo calculadora = new CalculadoraTempo();
         calculadora.inclui(meuFilmeBaseAula);
         calculadora.inclui(meuFilmeBaseAula2);
         calculadora.inclui(lost);
-        System.out.println(calculadora.getTempoTotal());
+       // System.out.println(calculadora.getTempoTotal());
 
         FiltroRecomenda filtro = new FiltroRecomenda();
-        filtro.filtra(meuFilmeBaseAula);
+      //  filtro.filtra(meuFilmeBaseAula);
         Episodio episidio = new Episodio();
         episidio.setNumero(1);
         episidio.setSerie(lost);
         episidio.setTotalVisualiza(300);
-        filtro.filtra(episidio);
+     //   filtro.filtra(episidio);
 
+        var filmeDoLuiz = new Filme();
+        filmeDoLuiz.setNome("Dog Ville");
+        filmeDoLuiz.setDuracaoEmMinutos(200);
+        filmeDoLuiz.setAnoLancamento(2000);
+        filmeDoLuiz.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoLuiz);
+        listaDeFilmes.add(meuFilmeBaseAula);
+        listaDeFilmes.add(meuFilmeBaseAula2);
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("Primeiro Filme: " + listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
+        System.out.println(listaDeFilmes.get(0).toString());
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -17,4 +17,9 @@ public class Filme extends Titulo implements Classifica {
     public int getClassifica() {
         return (int) obtemMedia() / 2;
     }
+
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + " (" + this.getAnoLancamento() + ")";
+    }
 }
